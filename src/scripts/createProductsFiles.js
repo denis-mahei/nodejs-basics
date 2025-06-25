04af3a943413e368b2d4bbd55a2e40534aeff2d5
denis-mahei <denmahei@gmail.com>
2025-06-25 15:35:25 +0300

    add filesPath dir

--- src/scripts/createProductsFiles.js
+++ src/scripts/createProductsFiles.js
@@ -1,5 +1,16 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { PATH_FILES_DIR } from '../constants/products.js';
 import { readProducts } from '../utils/readProducts.js';
 
 export const createProductsFiles = async () => {
   const data = await readProducts();
+  for (const datum of data) {
+    const fileName = datum.name.toLowerCase().split(' ').join('-');
+    const filePath = path.resolve(PATH_FILES_DIR, `${fileName}.json`);
+    await fs.writeFile(filePath, JSON.stringify(datum, null, 2));
+  }
+  console.log('Files created!🧾');
 };
+
+createProductsFiles();
